ed082c3c1a3bec04e701746f71fd648048d8d7a7
Pontus <contrib@pontusnetworks.com>
2018-08-27 11:52:11 +0100

    adding extra option to prevent elastic from complaining about low disk

--- build-local.sh
+++ build-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 VERSION=5.6.3-SNAPSHOT
 DISTDIR="$DIR/../pontus-dist/opt/pontus/pontus-elasticsearch";
 TARFILE=$DIR/distribution/tar/build/distributions/elasticsearch-${VERSION}.tar.gz
@@ -10,6 +10,7 @@ cd $DIR
 
 echo DIR is $DIR
 echo TARFILE is $TARFILE
+echo DISTDIR is $DISTDIR
 
 if [[ ! -f $TARFILE ]]; then
   gradle assemble
@@ -23,4 +24,6 @@ cd $DISTDIR
 rm -rf *
 tar xvfz $TARFILE
 ln -s elasticsearch-$VERSION current
+echo 'thread_pool.bulk.queue_size: 2000' >> conf/elasticsearch.yml
+echo 'cluster.routing.allocation.disk.threshold_enabled: false' >> conf/elasticsearch.yml
 cd $CURDIR
